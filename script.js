ac6192661267fbcb48b448c5096b0ac1606311d4
LineBreez <alinesn0308@gmail.com>
2022-01-27 00:00:35 -0300

    desafio4

---
+++ script.js
@@ -0,0 +1,46 @@
+//1 - crie uma função que exiba uma mensagem no console
+
+
+function helloJavaScript() {
+console.log("hello JavaScript!");
+}
+​
+helloJavaScript(); // helloJavaScript!
+
+//2 - crie uma função que receba o seu nome como (parâmetro) e exiba no console
+
+function nome {Aline)
+console.log(`Olá ${Aline}, seja bem vindo(a) ao Vai na Web!`);
+
+}
+//3 - crie uma função que receba nome, idade, e um estilo musical preferido (parâmetros) e exiba no console
+function nome {Aline)
+console.log(" Meu nome é {Aline Nogueira} e tenho" $(26) "meu estilo musical é {hop hop}" )
+
+function idade (26)
+console.log ("minha idade!")
+function Musica (Hip hop)
+console.log ("minha música preferida!")
+
+
+let aluno = {
+    nome:"Aline",
+    idade: 26,
+    Música: ["hip hop", "charme"]
+
+    // FIZ 3 JEITOS POR MITIVOS DE DUVIDA 
+
+//4 - crie uma função que receba um filme, uma música (parâmetros) e exiba no console
+
+function movie (movie) {
+    console.log("homem aranha");
+
+    function music (music)
+    console.log ("bolofofo-pão de queijo");
+}
+*Bônus:
+
+//5 - crie uma função que retorne o triplo do número recebido no parâmetro da função
+  
+function soma
+console.log ( )
